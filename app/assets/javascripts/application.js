de8248c90a8105be711c36bd716a80223155748f
jtobonb <jtobonb@outlook.com>
2014-10-25 19:42:21 -0500

    Actualizacion con jquery y bootstrap locales

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-1.11.1.min
+//= require bootstrap.min
 //= require jquery_ujs
-//= require_tree .
+= require_tree
