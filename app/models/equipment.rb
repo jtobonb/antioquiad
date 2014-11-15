22c258dc8f203f44718d2bd7e2d27bfc8a6c3d40
jtobonb <jtobonb@outlook.com>
2014-11-15 10:18:14 -0500

    Detalles de validacion

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,4 @@
 class Equipment < ActiveRecord::Base
   attr_accessible :dane_sede, :disco_duro, :display, :fabricante, :fecha_entrega, :memoria_ram, :modelo, :nombre, :origen, :procesador, :serial, :sist_operativo, :tipo
+  validates_presence_of :dane_sede, :disco_duro, :display, :fabricante, :fecha_entrega, :memoria_ram, :modelo, :nombre, :origen, :procesador, :serial, :sist_operativo, :tipo
 end
